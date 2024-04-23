d0ba0c899d6a32ef483f943b009fa7ec685c0ab1
yuanyuan6065 <1842755140@qq.com>
2024-04-23 16:57:17 +0800

    r

--- shanhai/src/main.js
+++ shanhai/src/main.js
@@ -1,11 +1,14 @@
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import router from './router' // 导入路由实例
 
 const app = createApp(App)
 
-app.use(router)
+app.use(ElementPlus) // 将 element 组件全局注册
+app.use(router) // 注册路由实例
 
 app.mount('#app')
--- shanhai/src/views/helloworld.vue
+++ shanhai/src/views/helloworld.vue
@@ -1,6 +1,18 @@
 <template>
-    <p>你好</p>
-</template>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200px">
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+        </el-aside>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+  
 
 
 <script>
